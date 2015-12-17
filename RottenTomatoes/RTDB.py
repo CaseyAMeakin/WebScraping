ee12c4b894965a501504c78fc3c20b2c16d0bbed
Casey Meakin <casey.meakin@gmail.com>
2015-12-16 22:55:23 -0800

    restructure some db handling

--- RottenTomatoes/RTDB.py
+++ RottenTomatoes/RTDB.py
@@ -3,17 +3,10 @@ Mostly wrappers for SQL queries on a database to store
 scraped Rotten Tomatoes data.
 """
 
-
 import sys
 from datetime import *
 from RottenTomatoes import *
-import sqlite3 as lite
-
-def escapeQuotes(string):
-    string = re.sub(u"\"",u"\"\"",unicode(string))
-    string = re.sub(u"\'",u"\'\'",unicode(string))
-    return string
-
+from db.sqlite3 import *
 
 def readMovieList(movie_list_file):
     movie_list = open(movie_list_file)
@@ -24,56 +17,6 @@ def readMovieList(movie_list_file):
         movies.append((year,title))
     return movies
 
-
-def connectDB(sqlite3_db_file_name):
-    con = lite.connect(sqlite3_db_file_name)
-    return con
-
-
-def updateTableRowKeyValueString(con,table,rowid,key,stringValue):
-    cur = con.cursor()
-    sql_ = u"""update {0} set {1} =  "{2}" where rowid = {3};"""
-    sqlcmd = sql_.format(table,key,stringValue,rowid)    
-    try:
-        results = cur.execute(sqlcmd).fetchall()
-    except:
-        print 'Error: ' + sqlcmd
-        results = None
-    con.commit()
-    return result
-
-
-def trySqlcmdFetchall(con,sqlcmd,logfile=None,quiet=True,logging=False):
-    cur = con.cursor()
-    try: 
-        results = cur.execute(sqlcmd).fetchall()
-        statCode = 'Success'
-    except:
-        results = None
-        statCode = 'Error'
-    logmsg = u"""[{0},{1},{2}]"""
-    msg = logmsg.format(datetime.now().isoformat(),sqlcmd,statCode)
-    if(logging): logfile.write(msg+"\n")
-    if(not quiet): print msg
-    return results
-
-
-def trySqlcmdCommit(con,sqlcmd,logfile=None,quiet=True,logging=False):
-    cur = con.cursor()
-    try:
-        cur.execute(sqlcmd)
-        statCode = 'Success'
-        con.commit()
-        rowid = cur.lastrowid
-    except:
-        if(not quiet): print 'ERROR: ' + sqlcmd
-        statCode = 'Error'
-        rowid = 0
-    logmsg = u"""[{0},{1},{2}]"""
-    msg = logmsg.format(datetime.now().isoformat(),sqlcmd,statCode)
-    if(logging): logfile.write(msg+"\n")
-    if(not quiet): print msg
-    return rowid
 
 def logRTDB(logfile,op,msg,quiet=True,doWrite=True):                                             
     """                                                                                          
